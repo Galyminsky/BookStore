147b9ae3a6c9ce95a422c4cfc3ee5f2764f1cf18
Galyaminsky_Note <jobforandroid@proton.me>
2024-05-26 11:27:28 +0500

    # !!Одноразовое обновление

--- app/src/main/java/me/proton/jobforandroid/bookstore/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/bookstore/MainActivity.kt
@@ -4,19 +4,98 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import me.proton.jobforandroid.bookstore.ui.theme.BookStoreTheme
+import me.proton.jobforandroid.bookstore.data.Book
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val fb = Firebase.firestore
-            fb.collection("books")
-                .document().set(mapOf("name" to "Proton"))
+            MainScreen()
+
         }
     }
 }
 
+@Composable
+fun MainScreen() {
+    val fb = Firebase.firestore
+    val list = remember {
+        mutableStateOf(emptyList<Book>())
+    }
+
+    fb.collection("books").get().addOnCompleteListener {  task ->
+        if (task.isSuccessful) {
+            list.value = task.result?.toObjects(Book::class.java)?: emptyList()
+        } else {
+            println("Error getting documents: ${task.exception}")
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f)
+        ) {
+            items(list.value) { book ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ) {
+                    Text(text = book.name, modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth()
+                        .padding(15.dp))
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            onClick = {
+                fb.collection("books")
+                    .document().set(
+                        Book(
+                            "Van Helsing",
+                            "Wow cool book",
+                            "300",
+                            "fantastic",
+                            "url"
+                        )
+                    )
+            }) {
+            Text(
+                text = "Add Book"
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+}
